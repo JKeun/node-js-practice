8b7b606d22ab2e8c5909b68f197728cc2f65f499
JKeun <jkeun1224@gmail.com>
2016-11-14 14:35:29 +0900

    init zigbang.js:
    using request module

--- zigbang.js
+++ zigbang.js
@@ -1,1 +1,7 @@
 var request = require('request');
+
+url = "http://api.zigbang.com/v1/items?detail=true&item_ids=3440910";
+
+request(url, function(error, res, body) {
+    console.log(JSON.parse(body));
+});
