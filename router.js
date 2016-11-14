6f17f35f16621dff47b959f0c39eebbffb3dce1f
JKeun <jkeun1224@gmail.com>
2016-11-15 00:03:20 +0900

    add roomApi router in router.js:
    require('request');

--- router.js
+++ router.js
@@ -6,6 +6,8 @@
 var express = require('express');
 var router = express.Router();
 
+var httpRequest = require('request');
+// 이렇게 이름을 바꿔서 가져오는 이유는 변수명 충돌을 막기 위해서
 
 router.get("/", function(req, res) {
     return res.send("HOME");
@@ -13,6 +15,16 @@ router.get("/", function(req, res) {
 
 router.get("/about/", function(req, res) {
     return res.send("ABOUT");
+});
+
+router.get("/rooms/:roomId/", function(req, res) {
+    var roomId = req.params.roomId;
+
+    var url = "https://api.zigbang.com/v1/items?detail=true&item_ids=" + roomId;
+
+    httpRequest(url, function(error, httpResponse, body) {
+        return res.send(JSON.parse(body));
+    });
 });
 
 module.exports = router;
