ec515b1331364fc8e06bcf708df56e3678ea0fa4
JKeun <jkeun1224@gmail.com>
2016-11-15 00:12:33 +0900

    add watchaApi router in router.js

--- router.js
+++ router.js
@@ -27,4 +27,14 @@ router.get("/rooms/:roomId/", function(req, res) {
     });
 });
 
+router.get("/watcha/", function(req, res) {
+    // 왓챠 영화 뉴스 정보가 뜨도록
+    // network 탭에서 news.json 으로 검색하면 API 주소가 있다.
+
+    var url = "https://watcha.net/home/news.json?page=2&per=20";
+    httpRequest(url, function(error, httpResponse, body) {
+        return res.send(JSON.parse(body));
+    });
+});
+
 module.exports = router;
