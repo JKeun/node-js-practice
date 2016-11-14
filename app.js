e08288d43ec1663e8a26cf0e7c579603d50783db
JKeun <jkeun1224@gmail.com>
2016-11-14 11:47:56 +0900

    init readcsv

---
+++ app.js
@@ -0,0 +1,18 @@
+// var csv = require(".csv");
+// csv("csv/students.csv"); 라고 했을 때,
+
+// => csv/students.csv 를 읽어서 javascript 객체 형식으로 전달해주는 모듈
+
+
+// module.exports = function(filePath) {
+ 
+// }  or
+
+
+function readCsv(filePath) {
+
+}
+
+module.exports = readCsv;
+
+readCsv("./csv/students");
