ebf2cfbd0b208178dc5ceac888d0607f73200c8f
JKeun <jkeun1224@gmail.com>
2016-11-21 17:18:43 +0900

    update users.js using readFileSync

---
+++ day2-http-get-post/routes/users.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var path = require('path');
+
+var express = require('express');
+var router = express.Router();
+
+
+router.get("/", function(req, res) {
+    var data = fs.readFileSync(
+        path.join(__dirname, "../db", "users.csv"),
+        {encoding:  "utf8"}        
+    );
+    var rows = data.split("\n");
+    var users = [];
+    row.forEach(function(row) {
+        var name = row.split(",")[0];
+        var email = row.split(",")[1];
+        var phonenumber = row.split(",")[2];
+
+        var user = {
+            name: name,
+            email: email,
+            phonenumber: phonenumber
+        };
+
+        users.push(user);
+    });
+
+    return res.render("users", {users: users});
+});
+
+module.exports = router;
