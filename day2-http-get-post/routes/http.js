2241a10e96ef30fa0248795adc9e0417c6b458ae
JKeun <jkeun1224@gmail.com>
2016-11-21 10:03:14 +0900

    add req.query

--- day2-http-get-post/routes/http.js
+++ day2-http-get-post/routes/http.js
@@ -3,11 +3,14 @@ var router = express.Router();
 
 
 router.get("/", function(req, res) {
-
+    // ...?key1=value1&key2=value2
+    // 처음 시작은 "?", 그 다음부터는 "&"
+    var data = req.query;
+    return res.send(data);
 });
 
 router.post("/", function(req, res) {
-
+    return res.send("POST!!!!");
 });
 
 
