914ac99c309c8b4452c966d79f5882b3840ca633
JKeun <jkeun1224@gmail.com>
2016-11-21 17:59:08 +0900

    add search function

--- day2-http-get-post/routes/users.js
+++ day2-http-get-post/routes/users.js
@@ -6,6 +6,8 @@ var router = express.Router();
 
 
 router.get("/", function(req, res) {
+    var search = req.query.search; // GET 데이터
+    
     var data = fs.readFileSync(
         path.join(__dirname, "../db", "users.csv"),
         {encoding:  "utf8"}        
@@ -26,7 +28,37 @@ router.get("/", function(req, res) {
         users.push(user);
     });
 
+    if (search) {
+        var matchedUserList = [];
+
+        users.forEach(function(user) {
+            if (user.name.indexOf(search) > -1) {
+                matchedUserList.push(user);
+            }
+        });
+
+        users = matchedUserList;
+    }
+
     return res.render("users", {users: users});
+});
+
+
+router.post("/", function(req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var phonenumber = req.body.phonenumber;
+
+    var newUserRow = name + "," + email + "," + phonenumber + "\n";
+
+    fs.appendFile(
+        path.join(__dirname, "../db", "users.csv"),
+        newUserRow,
+        function(error) {
+            return res.redirect("/users/");
+        }
+
+    );
 });
 
 module.exports = router;
