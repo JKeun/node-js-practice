40bf0733b72aebace02e7cac7eaf411dd6ccb3df
JKeun <jkeun1224@gmail.com>
2016-11-21 11:57:44 +0900

    add var query for search

--- day2-http-get-post/routes/watcha.js
+++ day2-http-get-post/routes/watcha.js
@@ -4,13 +4,16 @@ var router = express.Router();
 var httpRequest = require('request');
 
 router.get("/", function(req, res) {
+    var query = req.query.query;
+
     var url = "https://watcha.net/home/news.json?page=1&per=20";
     httpRequest(url, function(error, httpResponse, body) {
         var data = JSON.parse(body);
         var newsItems = data.news;
 
         var context = {
-            newsItems: newsItems
+            newsItems: newsItems,
+            query: query
         };
 
         return res.render("watcha", context);
