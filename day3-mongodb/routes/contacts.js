584316d8c9943b96018cc704764cb57db2ae41d0
JKeun <jkeun1224@gmail.com>
2016-12-01 11:19:58 +0900

    finish routes/contacts.js
    add views/contacts.pug

--- day3-mongodb/routes/contacts.js
+++ day3-mongodb/routes/contacts.js
@@ -2,13 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 
+var Contact = require("../models/contact");
+
+
 router.get("/", function(req, res) {
-    return res.send("contacts!");
+    var mongoQuery = {};
+
+    Contact.find(mongoQuery, function(error, contacts) {
+        return res.render("contacts", {contactItems: contacts});
+    });
 });
 
 
 router.post("/", function(req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var phonenumber = req.body.phonenumber;
 
+    var newContact = new Contact({
+        name: name,
+        email: email,
+        phonenumber: phonenumber
+    });
+
+    newContact.save(function(error) {
+        return res.redirect("/contacts/");
+    });
 });
 
 
