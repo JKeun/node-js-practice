900747d935d6a688ab34433000e28b1f799c1acf
JKeun <jkeun1224@gmail.com>
2016-12-01 10:46:01 +0900

    finish mongoQuery using regex

--- day3-mongodb/routes/movies.js
+++ day3-mongodb/routes/movies.js
@@ -5,7 +5,19 @@ var Movie = require("../models/movie");
 
 
 router.get("/", function(req, res) {
-    var mongoQuery = {}; // 이름 검색
+    var query = req.query.query || ""; // 만약에 없으면 모든 docs
+
+    // movies 라는  collection 에서
+    // title 에 만약 query ("드래곤볼") 포함된 모든 documents
+    // 정규표현식 => "____드래곤볼____"
+    // ".*드래곤볼.*"
+    // ".*" + query + ".*"
+    
+    var mongoQuery = {
+        title: {
+            $regex: ".*" + query + ".*"
+        }
+    }; // 이름 검색
 
     Movie.find(mongoQuery, function(error, movies) {
         return res.render("movies", {movieItems: movies});
