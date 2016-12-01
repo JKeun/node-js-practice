175660e131c03d5fabf1f6a9ee662f161c5d391e
JKeun <jkeun1224@gmail.com>
2016-12-01 09:30:43 +0900

    add request module in seed_movie.js

--- day3-mongodb/seed_movie.js
+++ day3-mongodb/seed_movie.js
@@ -1,20 +1,24 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 var Movie = require("./models/movie");
+
+var request = require('request');
+
 
 mongoose.connect("mongodb://mongodb.dobest.io/jkpark2");
 var db = mongoose.connection;
 
+
 db.once("open", function() {
     console.log("Database is connected");
 
-    var newMovie = new Movie({
-        title: "영화제목",
-        content: "영화본문",
-        url: "https://image.jpg"
-    });
-                
-    newMovie.save(function(error) {
-        if (error) throw error;
-        console.log("New Movie Added!");
+    request.get("https://watcha.net/home/news.json?page=1&per=100", function(error, response, body) {
+        var data = JSON.parse(body);
+        var newsItems = data.news;
+        console.log(newsItems.length);
+
+        // request 되기전에 db.close 되면 안되므로 이 안에다 코드작성함
+        db.close(function() {
+            console.log("Database is disconnected");
+        });
     });
 });
