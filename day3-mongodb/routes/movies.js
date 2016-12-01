449f3ecc0b13495b331ad5bba53a57a9f8703370
JKeun <jkeun1224@gmail.com>
2016-12-01 10:00:57 +0900

    init views/movies.pug

--- day3-mongodb/routes/movies.js
+++ day3-mongodb/routes/movies.js
@@ -6,7 +6,7 @@ var Movie = require("../models/movie");
 
 router.get("/", function(req, res) {
     Movie.find({}, function(error, movies) {
-        return response.render("movies", {movieItems: movies});
+        return res.render("movies", {movieItems: movies});
     });
 });
 
