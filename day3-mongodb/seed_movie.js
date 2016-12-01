ecb3b10af2c063524fdb91f5cf3a100c3c70fb44
JKeun <jkeun1224@gmail.com>
2016-12-01 09:43:30 +0900

    edit server to local mongo

--- day3-mongodb/seed_movie.js
+++ day3-mongodb/seed_movie.js
@@ -4,7 +4,7 @@ var Movie = require("./models/movie");
 var request = require('request');
 
 
-mongoose.connect("mongodb://mongodb.dobest.io/jkpark2");
+mongoose.connect("mongodb://localhost/nodecamp");
 var db = mongoose.connection;
 
 
@@ -14,11 +14,27 @@ db.once("open", function() {
     request.get("https://watcha.net/home/news.json?page=1&per=100", function(error, response, body) {
         var data = JSON.parse(body);
         var newsItems = data.news;
-        console.log(newsItems.length);
+        var movieItems = [];
 
-        // request 되기전에 db.close 되면 안되므로 이 안에다 코드작성함
-        db.close(function() {
-            console.log("Database is disconnected");
+        newsItems.forEach(function(newsItem) {
+            var movieItem = {
+                title: newsItem.title,
+                content: newsItem.content,
+                image: newsItem.image
+            };
+
+            movieItems.push(movieItem);
+        });
+
+        Movie.collection.insert(movieItems, function(error, movies) {
+            if (error) throw error;
+            console.log("Successfuly added");
+            
+            // request 되기전에 db.close 되면 안되므로 이 안에다 코드작성함
+            // insert 되기전에 close 되면 안되므로 이 안에다 코드 작성
+            db.close(function() {
+                console.log("Database is disconnected");
+            });
         });
     });
 });
