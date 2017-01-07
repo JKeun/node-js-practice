8ba1975f33d7befbd92fd7811f7f8e3e99be91a1
JKeun <jkeun1224@gmail.com>
2017-01-07 11:08:33 +0900

    add models/post.js

---
+++ day5-passport/models/post.js
@@ -0,0 +1,4 @@
+// 1. Post Model
+// 2. postRouter => posts:lst ( GET "/posts/" ), 
+//               => posts:create ( POST "/posts/" ), 
+//               => posts:new (GET "/posts/new/" => 새 글을 쓸 수 있는 form 을 render )
