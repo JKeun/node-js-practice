ed9406c867ff3a33729cc061ba68039b94a85a7e
JKeun <jkeun1224@gmail.com>
2017-01-08 11:48:45 +0900

    add postSchema in models/post.js

--- day5-passport/models/post.js
+++ day5-passport/models/post.js
@@ -1,4 +1,20 @@
+var mongoose = require('mongoose');
+
+
+var postSchema = mongoose.Schema({
+    title: String,
+    content: String
+});
+
+
+var Post = mongoose.model("Post", postSchema);
+
+
+module.exports = Post;
+
 // 1. Post Model
+
+
 // 2. postRouter => posts:lst ( GET "/posts/" ), 
 //               => posts:create ( POST "/posts/" ), 
 //               => posts:new (GET "/posts/new/" => 새 글을 쓸 수 있는 form 을 render )
