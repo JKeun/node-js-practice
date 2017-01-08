f08b8494cfe4e109cdfe876719583de485e377f7
JKeun <jkeun1224@gmail.com>
2017-01-08 11:57:43 +0900

    init router.route in routes/posts.js

--- day5-passport/routes/posts.js
+++ day5-passport/routes/posts.js
@@ -3,12 +3,23 @@ var router = express.Router();
 
 var Post = require("../models/post");
 
-
 // 2. postRouter => posts:lst ( GET "/posts/" ), 
 //               => posts:create ( POST "/posts/" ), 
 //               => posts:new (GET "/posts/new/" => 새 글을 쓸 수 있는 form 을 render )
 
+router.route("/")
+    .get(function(req, res) {  // posts:list
+    
+    })
+    .post(function(req, res) {  // posts:cerate
+    
+    });
 
+
+router.route("/new/")
+    .get(function(req, res) {  // posts:new
+    
+    });
 
 
 module.exports = router;
