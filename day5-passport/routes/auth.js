ac7b8e1b9052aea1187026c82006d30d00bb23c2
JKeun <jkeun1224@gmail.com>
2017-01-11 11:58:41 +0900

    add config/passport.js

--- day5-passport/routes/auth.js
+++ day5-passport/routes/auth.js
@@ -7,41 +7,11 @@ var authMiddleware = require("../middlewares/auth");
 
 
 var passport = require('passport');
-var passportLocal = require('passport-local');
-var passportLocalStrategy = passportLocal.Strategy;
-
-var passportKakao = require('passport-kakao');
-var passportKakaoStrategy = passportKakao.Strategy;
-
 // 1. authenticate = User.authenticate
 // 2. serialize ( user => user._id )
 // 3. deserialize ( userId => user )
 
 
-passport.use(new passportLocalStrategy(User.authenticate()));
-
-passport.serializeUser(User.serialize());
-passport.deserializeUser(User.deserialize());
-
-passport.use(new passportKakaoStrategy(
-    {
-        clientID: "580d5f3bd932796915950408014f8a81", // REST API
-        callbackURL: "http://localhost:3030/auth/kakao/callback"
-    },
-    function (accessToken, refreshToken, profile, next){
-        User.findOne({_idd: profile.id}, function(error, user) {
-            if (user) { return next(error, user); }
-            var user = new User({
-                username: profile.id,
-                password: profile.id  // password 가 있을 경우 대체 필요
-            })
-
-            user.save(function(error, user) {
-                return next(error, user);
-            });
-        })
-    }    
-));
 
 router.route("/login/")
     .get(function(req ,res) {
